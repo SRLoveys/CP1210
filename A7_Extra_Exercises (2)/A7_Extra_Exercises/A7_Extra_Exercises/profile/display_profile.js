c6453cc4c66a4acacef1f9c85bdc66a532df8185
SRLoveys <147052904+SRLoveys@users.noreply.github.com>
2024-12-02 23:03:00 -0330

    A7

---
+++ A7_Extra_Exercises (2)/A7_Extra_Exercises/A7_Extra_Exercises/profile/display_profile.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const $ = (selector) => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded",  () => {
+	// display data from session storage
+	
+	$("back").addEventListener("click", () => {
+		
+	}); // end of click()
+	
+}); // end of ready()
